ce74fa18aeff7f661714f37bd51d96fdce268639
drkphnx <dark.phnx12@gmail.com>
2023-07-12 08:11:45 +0530

    [squash]:ruby: Misound: Introduce MiDirac
     - Add a frontend for Dirac sound optimization settings ,Inject into sound preferences through settings IA.
    
    Signed-off-by: bvlj <bevilacquajoey@gmail.com>
    
    - Add DiracSound backend
    - Add support for hifi
    - Dont restore Dirac values on boot
      * Dirac libs are smart enough to store these values on power-off and restore them on boot.
    
    Signed-off-by: TheScarastic
    
    - Prevent NPE before DiracSound is initialized
      * There can be an edge case where the BOOT_COMPLETED intent is not yet received and user tries to open Dirac sound settings.
        Avoid letting it crash by making sure init is executed before showing the preference screen.
    
    Signed-off-by: Bruno Martins
    
    - Add summary for Dirac
    
    Signed-off-by: k001droid <k001droid.medium.com>
    
    - Dirac: Adapt to S style
     * Use Theme.SubSettingsBase for theme
     * Replace PreferenceActivity with CollapsingToolbarBaseActivity
     * Add Titles to prefernce screen
     * Remove onOptionsItemSelected and where neccessary move to activity
    
    Signed-off-by: TheScarastic
    Signed-off-by: johnmart19 <johnivan19999@gmail.com>
    
    - Disable hifi preference when toggling switchbar
     * Pause/play music stream to get effects applied
     * Rename setMusic to setEnabled
    
    Signed-off-by: jhenrique09 <jhsv09@gmail.com>
    
    - Add bass booster preset for MiSound
    
    Signed-off-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
    
    - Handle more error for dirac
    
    Signed-off-by: neobuddy89
    
    - Refactor dirac code
    [1] - Improve strings for dirac tile on/off
    [2] - Use proper singleton pattern for DiracUtils
    [3] - Describe current Dirac state to the tile
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: nullxception <enuma.alrizky@gmail.com>
    
    - Use TOGGLEABLE_TILE in Dirac
    [chrisw444: Adapt DiracTileService for that]
    
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: WSTxda <wstxda@gmail.com>
    
    - Enable hi-fi mode regardless headset and preset
    * Testing manually it seems hi-fi mode works regardless headset and preset value.
    
    Signed-off-by: neobuddy89
    
    - Add MiSound scenes
    
    Signed-off-by: lostark13 <tushar9915@gmail.com>
    
    - Add more Dirac presets
    * Presets Added: Live, Balanced, Bass Reduction, Treble Reduction, Soft Bass, Soft Treble
    
    Signed-off-by: mage1k99 <mage1k99@gmail.com>
    
    - Update and polish vector drawables
      Thanks to WSTxda for these icons
    
    Signed-off-by: WSTxda <wstxda@gmail.com>
    Signed-off-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
    
    - dirac: Add vector icons
    * from https://materialdesignicons.com/ with small changes
    
    Signed-off-by: ArianK16a
    
    - dirac: Update vector icons to MD2 versions
    
    Signed-off-by: Stallix
    
    - dirac: Add hifi vector icons
    * Icons taken from https://materialdesignicons.com/
    
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    
    - Change Headphone types for dirac
    * Update values according to misoundfx and drop non-existing values of dirac
    
    Signed-off-by: lostark13 <tushar9915@gmail.com>
    
    - dirac: Add some more dirac strings
    
    Signed-off-by: VEN0M98 <bt98.bt7@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
- *               2017-2019 The LineageOS Project
+ *               2017-2020 The LineageOS Project
+ *               2022-2023 VoidUI Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,12 +21,14 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.utils.FileUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
+
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
 
@@ -38,7 +41,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         } catch (Exception e) {
             Log.d(TAG, "Dirac is not present in system");
         }
-        ThermalUtils.startService(context);
-        RefreshUtils.startService(context);        
     }
 }
