0596e21a4c8c67cc4a705a4b2d9980aa4caf8f2a
RohitTiwari <rohitttiwari52@gmail.com>
2023-10-18 17:46:54 +0530

    ruby: overlays: Build missing overlays
    Signed-off-by: RohitTiwari <rohitttiwari52@gmail.com>

--- device.mk
+++ device.mk
@@ -255,7 +255,8 @@ PRODUCT_PACKAGES += \
     WifiResOverlayRuby
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
