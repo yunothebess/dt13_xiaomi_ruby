d0f1dcbd7d9e15625bf9f51b094d3f8b8050bc6c
yunothebess <yunothebess @users.noreply.github.com>
2023-10-22 18:55:02 +0000

    Ruby: Pixelifyy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,14 @@
 #
 # Copyright (C) 2023 The Alphadroid Project
 # Copyright (C) 2023 The EvolutionOS Project
+# Copyright (C) 2023 The PixelOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_ruby.mk \
+	$(LOCAL_DIR)/evolution_ruby.mk \
 	$(LOCAL_DIR)/evolution_ruby.mk
 
 COMMON_LUNCH_CHOICES := \
@@ -15,4 +17,7 @@ COMMON_LUNCH_CHOICES := \
     lineage_ruby-eng \
     evolution_ruby-user \
     evolution_ruby-userdebug \
-    evolution_ruby-eng 
+    evolution_ruby-eng \
+	aosp_ruby-user \
+    aosp_ruby-userdebug \
+    aosp_ruby-eng 
